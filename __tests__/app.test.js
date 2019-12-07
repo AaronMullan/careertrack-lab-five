c8ae0965e102cd301b604a14884a23edea92a18c
AaronMullan <aaroncmullan@gmail.com>
2019-12-06 17:44:17 -0800

    find all and find by Id working

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,5 +1,8 @@
+require('dotenv').config();
+require('../lib/utils/connect')();
 const request = require('supertest');
 const app = require('../lib/app');
+const Band = require('../Band.js');
 
 describe('application routes', () => {
   it('has a home route that says hello everyone', () => {
@@ -7,6 +10,72 @@ describe('application routes', () => {
       .get('/')
       .then(res => {
         expect(res.body).toEqual({ text: 'hello' });
+      });
+  });
+  it('has a post route', () => {
+    return request(app)
+      .post('/bands')
+      .send({
+        name: 'Black Sabbath',
+        drums: 'Bill Ward',
+        bass: 'Geezer Butler',
+        guitar: 'Toni Iommi',
+        canCountToFour: true
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          __v: expect.any(Number),
+          _id: expect.any(String),
+          name: 'Black Sabbath',
+          drums: 'Bill Ward',
+          bass: 'Geezer Butler',
+          guitar: 'Toni Iommi',
+          canCountToFour: true
+        });
+      });
+  });
+  it('has a get route to find one band by id', async() => {
+    const band = await Band.create({
+      name: 'Black Sabbath',
+      drums: 'Bill Ward',
+      bass: 'Geezer Butler',
+      guitar: 'Toni Iommi',
+      canCountToFour: true
+    });
+    return request(app)
+      .get(`/${band._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          __v: expect.any(Number),
+          _id: expect.any(String),
+          name: 'Black Sabbath',
+          drums: 'Bill Ward',
+          bass: 'Geezer Butler',
+          guitar: 'Toni Iommi',
+          canCountToFour: true
+        });
+      });
+  });
+  it('can find all bands', async() => {
+    const band = await Band.create({
+      name: 'Black Sabbath',
+      drums: 'Bill Ward',
+      bass: 'Geezer Butler',
+      guitar: 'Toni Iommi',
+      canCountToFour: true
+    });
+    return request(app)
+      .get('/band')
+      .then(res => {
+        expect(res.body[0]).toEqual({
+          __v: expect.any(Number),
+          _id: expect.any(String),
+          name: 'Black Sabbath',
+          drums: 'Bill Ward',
+          bass: 'Geezer Butler',
+          guitar: 'Toni Iommi',
+          canCountToFour: true
+        });
       });
   });
 });
